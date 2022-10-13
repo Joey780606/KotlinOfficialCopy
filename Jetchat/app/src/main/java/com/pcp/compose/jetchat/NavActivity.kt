596df28f0bf8b4219063d19bc72544a555867273
JoeyYang <joeyyang@pcpartner.com>
2022-10-13 09:04:38 +0800

    Feat Jetchat, modify NavActivity.kt
    Solution:
    1. Adjust NavActivity.kt
    2. Add BackHandler.kt
    
    Point:
    1. setContentView(
    2. ComposeView(this).apply {
    3. CompositionLocalProvider(
    4. LocalBackPressedDispatcher provides this@NavActivity.onBackPressedDispatcher 的 LocalBackPressedDispatcher
    5. 上方的 NavActivity.onBackPressedDispatcher

--- Jetchat/app/src/main/java/com/pcp/compose/jetchat/NavActivity.kt
+++ Jetchat/app/src/main/java/com/pcp/compose/jetchat/NavActivity.kt
@@ -8,24 +8,51 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import com.pcp.compose.jetchat.conversation.LocalBackPressedDispatcher
 import com.pcp.compose.jetchat.ui.theme.JetchatTheme
 
+/*
+    Author: Joey
+    Start time: 22/10/12
+    Point:
+      1. 重要的地方,加上 "重要:"
+      2. Git一次可放5個重點, 加上: "重點:"
+ */
 class NavActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            JetchatTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+
+        // Turn off the decor fitting system windows, which allows us to handle insets,
+        // including IME animations
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        setContentView(
+            ComposeView(this).apply {
+                //consumeWindowInsets = false  //重要: 這個先不加,看有無影響
+                setContent {
+                    CompositionLocalProvider(
+                        LocalBackPressedDispatcher provides this@NavActivity.onBackPressedDispatcher
+                    ) {
+
+                    }
+
+                    JetchatTheme {
+                        // A surface container using the 'background' color from the theme
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colorScheme.background
+                        ) {
+                            Greeting("Android")
+                        }
+                    }
                 }
             }
-        }
+        )
     }
 }
 
