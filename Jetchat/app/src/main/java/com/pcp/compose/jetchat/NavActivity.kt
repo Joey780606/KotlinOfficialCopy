ae29a0b88664547497e8ac1419684a0118a23251
JoeyYang <joeyyang@pcpartner.com>
2022-10-17 08:57:09 +0800

    Feat: Jetchat, adjust NavActivity.kt, MainViewModel.kt
    Solution:
    As title.
    
    Point:
    1. 加入 implementation "androidx.lifecycle:lifecycle-runtime-compose:2.6.0-alpha02"
    2. private val viewModel: MainViewModel by viewModels()
    3. @OptIn(ExperimentalMaterial3Api::class, ExperimentalLifecycleComposeApi::class) ,為了以下
      4. val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
      5. val drawerOpen by viewModel.drawerShouldBeOpened.collectAsStateWithLifecycle()
    6. private val _drawerShouldBeOpened = MutableStateFlow(false)
       val drawerShouldBeOpened: StateFlow<Boolean> = _drawerShouldBeOpened
    
    Author: Joey

--- Jetchat/app/src/main/java/com/pcp/compose/jetchat/NavActivity.kt
+++ Jetchat/app/src/main/java/com/pcp/compose/jetchat/NavActivity.kt
@@ -3,18 +3,21 @@ package com.pcp.compose.jetchat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
+import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.pcp.compose.jetchat.conversation.LocalBackPressedDispatcher
 import com.pcp.compose.jetchat.ui.theme.JetchatTheme
+import com.pcp.compose.jetchat.ui.theme.MainViewModel
 
 /*
     Author: Joey
@@ -24,6 +27,9 @@ import com.pcp.compose.jetchat.ui.theme.JetchatTheme
       2. Git一次可放5個重點, 加上: "重點:"
  */
 class NavActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
+
+    @OptIn(ExperimentalMaterial3Api::class, ExperimentalLifecycleComposeApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -38,7 +44,9 @@ class NavActivity : ComponentActivity() {
                     CompositionLocalProvider(
                         LocalBackPressedDispatcher provides this@NavActivity.onBackPressedDispatcher
                     ) {
-
+                        val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)    //因為這個,所以需要加入 @OptIn(ExperimentalMaterial3Api::class)
+                        val drawerOpen by viewModel.drawerShouldBeOpened.collectAsStateWithLifecycle() //重要: 這要 implementation "androidx.lifecycle:lifecycle-runtime-compose:2.6.0-alpha01"
+                        // 且 collectAsStateWithLifecycle() 在 onCreate 要加入 ExperimentalLifecycleComposeApi::class
                     }
 
                     JetchatTheme {
---
+++ Jetchat/app/src/main/java/com/pcp/compose/jetchat/ui/theme/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.pcp.compose.jetchat.ui.theme
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class MainViewModel : ViewModel() {
+    private val _drawerShouldBeOpened = MutableStateFlow(false)
+    val drawerShouldBeOpened: StateFlow<Boolean> = _drawerShouldBeOpened
+}
