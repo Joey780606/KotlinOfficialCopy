af8655af47fcf8fc6ec7233ecd3c5e238d9df3f0
Joey <yty0606@gmail.com>
2022-10-20 15:18:26 +0800

    Feat: Reply, Join Flow, and WindowStateUtils.kt
    Solution:
    1. Adjust related program.
    
    Point:
    1. WindowInfoTracker.getOrCreate(this).windowLayoutInfo(this)
      .flowWithLifecycle(this.lifecycle)
      .map { layoutInfo -> 
      .stateIn(
    2. sealed interface DevicePosture {
    3. fun isBookPosture(foldFeature: FoldingFeature?): Boolean {
    4.  contract { returns(true) implies (foldFeature != null) }
    
    Author: Joey

--- Reply/app/src/main/java/com/pcp/replycopy/MainActivity.kt
+++ Reply/app/src/main/java/com/pcp/replycopy/MainActivity.kt
@@ -13,7 +13,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.window.layout.FoldingFeature
+import androidx.window.layout.WindowInfoTracker
 import com.pcp.replycopy.ui.theme.ReplyCopyTheme
+import com.pcp.replycopy.ui.utils.DevicePosture
+import com.pcp.replycopy.ui.utils.isBookPosture
+import com.pcp.replycopy.ui.utils.isSeparating
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+/*
+    Author: Joey
+    Start time: 22/10/12
+    Point:
+      1. 重要的地方,加上 "重要:"
+      2. Git一次可放5個重點, 加上: "重點:"
+ */
 
 class MainActivity : ComponentActivity() {
 
@@ -22,6 +40,31 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)  //calculateWindowSizeClass 需要
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Flow of [DevicePosture] that emits every time there's a change in the windowLayoutInfo
+        val devicePostureFlow = WindowInfoTracker.getOrCreate(this).windowLayoutInfo(this) //重要: 要 implementation androidx.window
+            .flowWithLifecycle(this.lifecycle)  //重要: 需要 import kotlinx.coroutines.flow.SharingStarted
+            .map { layoutInfo ->  //重要: 需要 import kotlinx.coroutines.flow.map
+                val foldingFeature =
+                    layoutInfo.displayFeatures
+                        .filterIsInstance<FoldingFeature>()  //重要: 需要 import androidx.window.layout.FoldingFeature
+                        .firstOrNull()
+                when {
+                    isBookPosture(foldingFeature) ->    //utils > WindowStateUtils.kt
+                        DevicePosture.BookPosture(foldingFeature.bounds)
+
+                    isSeparating(foldingFeature) ->
+                        DevicePosture.Separating(foldingFeature.bounds, foldingFeature.orientation)
+
+                    else -> DevicePosture.NormalPosture
+                }
+            }
+            .stateIn( //重要: 需要 import kotlinx.coroutines.flow.stateIn
+                scope = lifecycleScope,
+                started = SharingStarted.Eagerly,
+                initialValue = DevicePosture.NormalPosture
+            )
+
         setContent {
             ReplyCopyTheme {
                 val windowSize = calculateWindowSizeClass(this)
---
+++ Reply/app/src/main/java/com/pcp/replycopy/ui/utils/WindowStateUtils.kt
@@ -0,0 +1,35 @@
+package com.pcp.replycopy.ui.utils
+
+import android.graphics.Rect
+import androidx.window.layout.FoldingFeature
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
+
+/**
+ * Information about the posture of the device
+ */
+sealed interface DevicePosture {
+    object NormalPosture : DevicePosture
+
+    data class BookPosture(
+        val hingePosition: Rect
+    ) : DevicePosture
+
+    data class Separating(
+        val hingePosition: Rect,
+        var orientation: FoldingFeature.Orientation
+    ) : DevicePosture
+}
+
+@OptIn(ExperimentalContracts::class)
+fun isBookPosture(foldFeature: FoldingFeature?): Boolean {
+    contract { returns(true) implies (foldFeature != null) } //重要: contract 需要 @OptIn(ExperimentalContracts::class)
+    return foldFeature?.state == FoldingFeature.State.HALF_OPENED &&
+            foldFeature.orientation == FoldingFeature.Orientation.VERTICAL
+}
+
+@OptIn(ExperimentalContracts::class)
+fun isSeparating(foldFeature: FoldingFeature?): Boolean {
+    contract { returns(true) implies (foldFeature != null) } //重要: contract 需要 @OptIn(ExperimentalContracts::class)
+    return foldFeature?.state == FoldingFeature.State.FLAT && foldFeature.isSeparating
+}
